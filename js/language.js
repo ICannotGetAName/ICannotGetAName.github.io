585a1c0cb37fa00beec29640d46a76bf6cc41aa5
ICannotGetAName <106532788+ICannotGetAName@users.noreply.github.com>
2025-03-28 13:27:34 +0100

    Update language.js

--- js/language.js
+++ js/language.js
@@ -1,11 +1,11 @@
 i18next
-  .use(i18nextHttpBackend)
+  .use(i18nextHttpBackend) // ← 你可能忘了这一行！
   .use(i18nextBrowserLanguageDetector)
   .init({
     debug: true,
     fallbackLng: 'en',
     backend: {
-      loadPath: 'locales/{{lng}}.json'  // ← 注意这里没有 "../"，因为 index.html 在根目录
+      loadPath: 'locales/{{lng}}.json' // ← 这个路径对 index.html 是正确的
     }
   }, function(err, t) {
     updateContent();
@@ -21,7 +21,7 @@ function updateContent() {
 
 function changeLanguage(language) {
   i18next.changeLanguage(language, function (err, t) {
-    if (err) return console.error('Language loading error:', err);
+    if (err) return console.error('Something went wrong loading', err);
     updateContent();
   });
 }
