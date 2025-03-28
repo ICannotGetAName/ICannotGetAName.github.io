341fdff847a7506a85633fb475144a1d53c6b000
ICannotGetAName <wxinrui455@gmail.com>
2025-03-28 12:56:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ICannotGetAName.github.io
+
+this is my own blog&info website which is written 100% by myself
+
+In the website you might see my education, career, about me and so on.
---
+++ js/language.js
@@ -0,0 +1,27 @@
+i18next
+  .use(i18nextHttpBackend)
+  .use(i18nextBrowserLanguageDetector)
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    backend: {
+      loadPath: '../locales/{{lng}}.json'
+    }
+  }, function(err, t) {
+    updateContent();
+  });
+
+function updateContent() {
+  document.querySelectorAll('[data-i18n]').forEach(el => {
+    const key = el.getAttribute('data-i18n');
+    const value = i18next.t(key);
+    if (value) el.innerText = value;
+  });
+}
+
+function changeLanguage(language) {
+  i18next.changeLanguage(language, function (err, t) {
+    if (err) return console.error('Something went wrong loading', err);
+    updateContent();
+  });
+}
